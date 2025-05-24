e6a4a0dee2d1317c80dafd135a70383905926e72
Karanpong2211310079 <tankangamer@gmail.com>
2025-05-24 16:31:44 +0700

    Fix API endpoint in login component by removing trailing slash

--- src/User/login.jsx
+++ src/User/login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const res = await axios.post(`${API_BASE_URL}/login`, {
+      const res = await axios.post(`${API_BASE_URL}login`, {
         name,
         phone,
       });
